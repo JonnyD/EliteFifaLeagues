4b00d1537c552e1711f88fbbf22a6c30fa547ad2
Jonathan Devine <contact@jonnydevine.com>
2017-08-13 22:44:52 +0100

    StandingRepository

---
+++ src/EliteFifa/StandingBundle/Criteria/StandingCriteria.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace EliteFifa\StandingBundle\Criteria;
+
+use EliteFifa\CompetitionBundle\Entity\Competition;
+use EliteFifa\SeasonBundle\Entity\Season;
+
+class StandingCriteria
+{
+    /**
+     * @var string
+     */
+    private $tableType;
+
+    /**
+     * @var string
+     */
+    private $standingType;
+
+    /**
+     * @var Competition
+     */
+    private $competition;
+
+    /**
+     * @var Season
+     */
+    private $season;
+
+    /**
+     * @return string
+     */
+    public function getTableType()
+    {
+        return $this->tableType;
+    }
+
+    /**
+     * @param string $tableType
+     */
+    public function setTableType($tableType)
+    {
+        $this->tableType = $tableType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStandingType()
+    {
+        return $this->standingType;
+    }
+
+    /**
+     * @param string $standingType
+     */
+    public function setStandingType($standingType)
+    {
+        $this->standingType = $standingType;
+    }
+
+    /**
+     * @return Competition
+     */
+    public function getCompetition()
+    {
+        return $this->competition;
+    }
+
+    /**
+     * @param Competition $competition
+     */
+    public function setCompetition(Competition $competition)
+    {
+        $this->competition = $competition;
+    }
+
+    /**
+     * @return Season
+     */
+    public function getSeason()
+    {
+        return $this->season;
+    }
+
+    /**
+     * @param Season $season
+     */
+    public function setSeason(Season $season)
+    {
+        $this->season = $season;
+    }
+}
---
+++ src/EliteFifa/StandingBundle/Repository/StandingRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace EliteFifa\StandingBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use EliteFifa\StandingBundle\Criteria\StandingCriteria;
+use EliteFifa\StandingBundle\Entity\Standing;
+
+class StandingRepository extends EntityRepository
+{
+    /**
+     * @param StandingCriteria $criteria
+     * @return Standing[]
+     */
+    public function findStandingsByCriteria(StandingCriteria $criteria)
+    {
+        $qb = $this->findStandingsByCriteriaQueryBuilder($criteria);
+
+        $query = $qb->getQuery();
+        $result = $query->getResult();
+
+        return $result;
+    }
+
+    /**
+     * @param StandingCriteria $criteria
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function findStandingsByCriteriaQueryBuilder(StandingCriteria $criteria)
+    {
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+
+        $qb->select('standing')
+            ->from('DW\StandingBundle\Entity\Standing', 'standing');
+
+        if ($criteria->getTableType()) {
+            $qb->andWhere('standing.tableType = :tableType')
+                ->setParameter('tableType', $criteria->getTableType());
+        }
+
+        if ($criteria->getStandingType()) {
+            $qb->andWhere('standing.standingType = :standingType')
+                ->setParameter('standingType', $criteria->getStandingType());
+        }
+
+        if ($criteria->getCompetition()) {
+            $qb->andWhere('standing.competition = :competition')
+                ->setParameter('competition', $criteria->getCompetition());
+        }
+
+        if ($criteria->getSeason()) {
+            $qb->andWhere('standing.season = :season')
+                ->setParameter('season', $criteria->getSeason());
+        }
+
+        return $qb;
+    }
+}
